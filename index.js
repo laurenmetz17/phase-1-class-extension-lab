3aad7308c8b397c8fc0073ff088be59281ae85b7
Lauren Metz <Laurenmetz15@gmail.com>
2022-08-09 12:01:56 -0400

    commit tree 39de9c2092db18959c9258842854a1d292b79be1

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Your code here
+class Polygon {
+    constructor(ints) {
+        this.ints = ints;
+    }
+
+    get countSides() {
+        return this.ints.length;
+    }
+
+    get perimeter() {
+        let sum = 0;
+        this.ints.forEach(side => {
+            sum = sum + side;
+        });
+        return sum;
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        if(this.ints[0] + this.ints[1] <= this.ints[2]){
+            return false;
+        }
+        else if(this.ints[1] + this.ints[2] <= this.ints[0]) {
+            return false;
+        }
+        else if(this.ints[0] + this.ints[2] <= this.ints[1]) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+}
+class Square extends Polygon {
+    get isValid() {
+        if(this.ints[0] == this.ints[1] && this.ints[0] == this.ints[2] && this.ints[0] == this.ints[3]) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    get area() {
+        if(this.isValid == true) {
+            return this.ints[0] * this.ints[1];
+        }
+        else { return 'not a square'}
+    }
+}
